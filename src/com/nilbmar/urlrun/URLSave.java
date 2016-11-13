076815fdd0a327a3cdceb025521db0eede5db031
Nilbmar <Nilbmar@gmail.com>
2016-11-12 21:25:54 -0500

    Reverted change in Eclipse EGit

--- src/com/nilbmar/urlrun/URLSave.java
+++ src/com/nilbmar/urlrun/URLSave.java
@@ -221,8 +221,6 @@ public class URLSave extends Application {
 		
 		// Doesn't currently work because they aren't recounted
 		getNewSize();
-		
-		// TODO: TESTING GIT CHANGES
 	}
 	
 	public static void main(String[] args) {
